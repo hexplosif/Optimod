e89fbca66beea6de40becce536a2c1510dee4e44
AdSchl2E <adam.schlee@insa-lyon.fr>
2024-11-27 09:15:49 +0100

    Init

---
+++ src/main/java/com/hexplosif/optimod/OptimodApplication.java
@@ -0,0 +1,13 @@
+package com.hexplosif.optimod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptimodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hexplosif/optimod/OptimodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexplosif.optimod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptimodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
