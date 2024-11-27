0f68a1428198d6aeecf7cb70750fb481fe2c94d1
mbonkoungou <mathis.bonkoungou@insa-lyon.fr>
2024-11-27 10:28:18 +0100

    loadDeliveryRequest modifié

--- src/main/java/com/hexplosif/controller/OptimodController.java
+++ src/main/java/com/hexplosif/controller/OptimodController.java
@@ -1,11 +1,63 @@
-package com.hexplosif.controller;
+import java.util.ArrayList;
+import java.util.List;
+
+package controller;
 
 public class OptimodController {
 
     public void loadMap(String XMLfilename) {
         // Load the map from the XML file
     }
-    public void loadDeliveries(String XMLfilename) {
-        // Load the deliveries from the XML file
+    public void loadDeliveryRequest(String XMLfilename) {
+
+        try {
+            // Load the deliveries from the XML file
+            File fichierXML = new File("../ressources/" + XMLfilename);
+
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+
+            DocumentBuilder builder = factory.newDocumentBuilder();
+
+            Document document = builder.parse(fichierXML);
+
+            document.getDocumentElement().normalize();
+
+            String nomRacine = document.getDocumentElement().getNodeName();
+            if (!"demandeDeLivraisons".equals(nomRacine)) {
+                System.err.println("Erreur : le nom de l'élément racine n'est pas 'demandeDeLivraisons'.");
+                System.exit(1);
+            }
+
+            NodeList listeLivraisons = document.getElementsByTagName("livraison");
+
+            // Liste pour stocker les tuples (enlèvement, livraison)
+            List<String[]> adresses = new ArrayList<>();
+
+            for (int i = 0; i < listeLivraisons.getLength(); i++) {
+                Node livraison = listeLivraisons.item(i);
+
+                if (livraison.getNodeType() == Node.ELEMENT_NODE) {
+                    Element elementLivraison = (Element) livraison;
+
+                    // Extraire les attributs adresseEnlevement et adresseLivraison
+                    String adresseEnlevement = elementLivraison.getAttribute("adresseEnlevement");
+                    String adresseLivraison = elementLivraison.getAttribute("adresseLivraison");
+
+                    // Ajouter dans la liste sous forme de tableau
+                    adresses.add(new String[]{adresseEnlevement, adresseLivraison});
+                }
+            }
+
+            // Afficher les résultats
+            System.out.println("Tuples (enlèvement, livraison) :");
+            for (String[] tuple : adresses) {
+                System.out.println("Enlèvement : " + tuple[0] + ", Livraison : " + tuple[1]);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
+
